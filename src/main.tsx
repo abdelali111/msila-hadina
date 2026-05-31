d30d22e194941bf52de22afc1b142451345696f4
Abdelali <abdelali111@github.com>
2026-05-31 00:38:41 -0700

    Fix React rendering error by using RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { startInstance } from "./start";
+import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
 
-async function render() {
-  const router = getRouter();
+const router = getRouter();
 
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-      <router.Provider>{startInstance}</router.Provider>
-    </React.StrictMode>
-  );
-}
-
-render().catch(console.error);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
